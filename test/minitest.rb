419859069d35ab01d41af36275089858b7aaacad
Alberto Ochoa <alberto.ochoa@ibcinc.com>
2020-03-05 16:52:41 -0500

    sweet and simple minitest to confirm the send_text function works as expected.

---
+++ test/minitest.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require_relative 'globals'
+
+describe "I can open a web browser and write?" do
+  before do
+    dp = File.expand_path( $driver_path )
+    puts "Driver Path: #{ dp }"
+    Selenium::WebDriver::Chrome::Service.driver_path= "#{ dp }/chromedriver"
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument( '--start-maximized' )
+    $driver = Selenium::WebDriver.for :chrome, options: options
+  end
+
+  describe "it can read and write" do
+    it "can write account_number" do
+      $driver.navigate.to "file:///data/work/webbill/index.html"
+      is_displayed( :id, 'ship_account' ).must_equal true
+      send_text :id, 'ship_account', '8040'
+      send_text :id, 'ship_reference', '111234'
+      get_input_value( :id, 'ship_account' ).must_equal '8040'
+    end
+  end
+
+end
